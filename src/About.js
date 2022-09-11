a33fa9b2b82386ffe8b1af13ff76148a8efb5dc7
Tom Officer <thomasofficerjr@gmail.com>
2022-09-11 03:11:01 -0700

    added about section content

--- src/About.js
+++ src/About.js
@@ -9,81 +9,86 @@ import {
   Icon,
   useColorModeValue,
   createIcon,
+  Image,
+  Center,
+  VStack,
 } from '@chakra-ui/react';
-import Header from './Header';
+import HeaderAlternate from './HeaderAlternate';
 import Footer from './Footer';
 
 const About = () => {
   return (
     <>
-      <Header />
-      <Container maxW={'3xl'}>
-        <Stack
-          as={Box}
-          textAlign={'center'}
-          spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}
+      <HeaderAlternate />
+
+      <Box bg={'yellow.300'} w={'full'}>
+        <Box
+          w={'full'}
+          backgroundImage="url('https://ik.imagekit.io/e16xo45jw/Blog/vipul-jha-a4X1cdC1QAc-unsplash_niPBlIeA7.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1662870890832')"
+          backgroundPosition={'center'}
+          backgroundSize="cover"
         >
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
-            lineHeight={'110%'}
-          >
-            Make money from <br />
-            <Text as={'span'} color={'green.400'}>
-              your audience
-            </Text>
-          </Heading>
-          <Text color={'gray.500'}>
-            Monetize your content by charging your most loyal readers and reward
-            them loyalty points. Give back to your loyal readers by granting
-            them access to your pre-releases and sneak-peaks.
-          </Text>
-          <Stack
-            direction={'column'}
-            spacing={3}
-            align={'center'}
-            alignSelf={'center'}
-            position={'relative'}
-          >
-            <Button
-              colorScheme={'green'}
-              bg={'green.400'}
-              rounded={'full'}
-              px={6}
-              _hover={{
-                bg: 'green.500',
-              }}
+          <Container maxW={'3xl'}>
+            <Stack
+              as={Box}
+              textAlign={'center'}
+              spacing={{ base: 8, md: 14 }}
+              py={{ base: 20, md: 20 }}
             >
-              Get Started
-            </Button>
-            <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
-              Learn more
-            </Button>
-            <Box>
-              <Icon
-                as={Arrow}
-                color={useColorModeValue('gray.800', 'gray.300')}
-                w={71}
-                position={'absolute'}
-                right={-71}
-                top={'10px'}
-              />
-              <Text
-                fontSize={'lg'}
-                fontFamily={'Caveat'}
-                position={'absolute'}
-                right={'-125px'}
-                top={'-15px'}
-                transform={'rotate(10deg)'}
+              <Heading
+                fontWeight={600}
+                fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+                lineHeight={'110%'}
               >
-                Starting at $15/mo
+                <Box>
+                  <Center>
+                    <VStack>
+                      <Text fontSize={'60px'} mb={'20px'} color={'white'}>
+                        Thank you for visiting
+                      </Text>
+                      <Image
+                        src="https://ik.imagekit.io/e16xo45jw/Blog/jsct-logo-white_57YEipdMW.png?ik-sdk-version=javascript-1.4.3&updatedAt=1662871696512"
+                        w={'400px'}
+                      />
+                    </VStack>
+                  </Center>
+                </Box>
+              </Heading>
+
+              <Text color={'white'} fontSize="18px">
+                Hey, I'm Tom, thanks for checking out my blog! <br /> I'm a full
+                stack developer from Northern California with a bachelor's
+                degree in Audio Engineering. I currently work remotely from
+                beautiful Northern California as a Frontend Developer for a
+                company based out of Los Angeles. I created this blog to share
+                solutions to challenges I've came across in my career and to
+                share information about topics that have little pre-exisiting
+                documentation. I hope you can find it of use! <br />
+                <br /> Before I fell in love with the world of programming I
+                spent many years working as a live sound engineer and touring
+                with bands. I am also a musician who plays a few bands out here
+                in California and a father to an amazing deaf child. When I'm
+                not building websites I spend the little free time I have
+                practicing American Sign Language, playing gigs with my band and
+                enjoying the wilderness of beautiful Northern California with my
+                two dogs. <br /> <br />
+                Thanks for visiting JavaScript Coding Tips and stay tuned for
+                new content weekly! Please don't hesitate to reach out or
+                connect with me on any of my social media links. <br /> I love
+                making new connections and am always open to new opportunities!
+                <br /> <br />
+                Cheers!
               </Text>
-            </Box>
-          </Stack>
-        </Stack>
-      </Container>
-      <Footer />
+            </Stack>
+          </Container>
+        </Box>
+        <Box bg={'yellow.300'}>
+          <Center>
+            <Image src="https://ik.imagekit.io/e16xo45jw/Blog/js1a_lcOMm6WvG.png?ik-sdk-version=javascript-1.4.3&updatedAt=1662842125322" />
+          </Center>
+        </Box>
+        <Footer />
+      </Box>
     </>
   );
 };
---
+++ src/HeaderAlternate.js
@@ -0,0 +1,228 @@
+import React, { useRef } from 'react';
+import {
+  useColorModeValue,
+  useDisclosure,
+  useColorMode,
+  Link,
+  chakra,
+  Box,
+  SimpleGrid,
+  Stack,
+  VStack,
+  Button,
+  CloseButton,
+  Flex,
+  HStack,
+  Spacer,
+  IconButton,
+  Center,
+} from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+const Header = ({ blogHandle, homeHandle, signUpHandle }) => {
+  const bg = useColorModeValue('yellow.300', 'black');
+  const cl = useColorModeValue('black', 'white');
+  const headerText = useColorModeValue('black', 'yellow.300');
+  const headerTextHover = useColorModeValue('white', 'white');
+  const mobileNav = useDisclosure();
+  const { toggleColorMode: toggleMode } = useColorMode();
+  const text = useColorModeValue('dark', 'light');
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+  const ic = useColorModeValue('brand.600', 'brand.50');
+  const hbg = useColorModeValue('gray.50', 'brand.400');
+  const tcl = useColorModeValue('gray.900', 'gray.50');
+  const dcl = useColorModeValue('gray.500', 'gray.50');
+  const hbgh = useColorModeValue('gray.100', 'brand.500');
+
+  const MobileNavContent = (
+    <VStack
+      pos="absolute"
+      top={0}
+      left={0}
+      right={0}
+      display={mobileNav.isOpen ? 'flex' : 'none'}
+      flexDirection="column"
+      p={2}
+      pb={4}
+      m={2}
+      bg={bg}
+      spacing={3}
+      rounded="sm"
+      shadow="sm"
+    >
+      <CloseButton
+        aria-label="Close menu"
+        justifySelf="self-start"
+        onClick={mobileNav.onClose}
+      />
+      <Button w="full" variant="ghost">
+        Dashboard
+      </Button>
+      <Button w="full" variant="solid" colorScheme="brand">
+        Inbox
+      </Button>
+      <Button w="full" variant="ghost">
+        Videos
+      </Button>
+    </VStack>
+  );
+
+  //JSX Return
+  return (
+    <React.Fragment>
+      <Box position="sticky" top={0} borderBottom="1px solid black">
+        <chakra.header
+          h="full"
+          bg={bg}
+          _hover={{
+            bg: 'white',
+          }}
+          w="full"
+          px={{
+            base: 2,
+            sm: 4,
+          }}
+          py={4}
+        >
+          <Flex alignItems="center" justifyContent="space-between" mx="auto">
+            <Link display="flex" alignItems="center" href="/">
+              {/* <Logo /> */}
+            </Link>
+            <Box
+              display={{
+                base: 'none',
+                md: 'inline-flex',
+              }}
+            >
+              <HStack spacing={1}>
+                <Box role="group"></Box>
+                <a href="/">
+                  <Button
+                    bg="none"
+                    color={headerText}
+                    display="inline-flex"
+                    alignItems="center"
+                    fontSize="md"
+                    _hover={{
+                      transform: 'scale(1.1)',
+                    }}
+                    _focus={{
+                      boxShadow: 'none',
+                    }}
+                  >
+                    Home
+                  </Button>
+                </a>
+              </HStack>
+            </Box>
+            <Spacer />
+
+            <Box display="flex" alignItems="center">
+              <a href="/resources">
+                <Button
+                  bg="none"
+                  color={headerText}
+                  display="inline-flex"
+                  alignItems="center"
+                  fontSize="md"
+                  _hover={{
+                    transform: 'scale(1.1)',
+                  }}
+                  _focus={{
+                    boxShadow: 'none',
+                  }}
+                >
+                  Resources
+                </Button>
+              </a>
+              <a href="/about">
+                <Button
+                  bg="none"
+                  color={headerText}
+                  display="inline-flex"
+                  alignItems="center"
+                  fontSize="md"
+                  _hover={{
+                    transform: 'scale(1.1)',
+                  }}
+                  _focus={{
+                    boxShadow: 'none',
+                  }}
+                >
+                  About
+                </Button>
+              </a>
+              <a href="/contact">
+                <Button
+                  bg="none"
+                  color={headerText}
+                  display="inline-flex"
+                  alignItems="center"
+                  fontSize="md"
+                  _hover={{
+                    transform: 'scale(1.1)',
+                  }}
+                  _focus={{
+                    boxShadow: 'none',
+                  }}
+                >
+                  Contact
+                </Button>
+              </a>
+            </Box>
+
+            <Box display="flex" alignItems="center" ml={5}>
+              <HStack spacing={1}>
+                <Button
+                  colorScheme="brand"
+                  variant="solid"
+                  size="sm"
+                  color="white"
+                  bg={'black'}
+                  _hover={{
+                    color: 'yellow.300',
+                    transform: 'scale(1.1)',
+                  }}
+                >
+                  Subscribe
+                </Button>
+              </HStack>
+              <IconButton
+                size="md"
+                fontSize="lg"
+                aria-label={`Switch to ${text} mode`}
+                variant="ghost"
+                color="current"
+                ml={{
+                  base: '0',
+                  md: '3',
+                }}
+                onClick={toggleMode}
+                icon={<SwitchIcon />}
+              />
+              <IconButton
+                display={{
+                  base: 'flex',
+                  md: 'none',
+                }}
+                aria-label="Open menu"
+                fontSize="20px"
+                color="gray.800"
+                _dark={{
+                  color: 'inherit',
+                }}
+                variant="ghost"
+                // icon={<AiOutlineMenu />}
+                onClick={mobileNav.onOpen}
+              />
+            </Box>
+          </Flex>
+
+          {MobileNavContent}
+        </chakra.header>
+      </Box>
+    </React.Fragment>
+  );
+};
+
+export default Header;
