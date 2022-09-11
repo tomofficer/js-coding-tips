f4134bd7af6ca662823ad4f7417f15c98704792f
Tom Officer <thomasofficerjr@gmail.com>
2022-09-11 02:03:54 -0700

    implemented react router, implemented new paths

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
 import React, { useRef } from 'react';
-import { ChakraProvider, Box, VStack, Grid, theme } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import Header from './Header';
-import Footer from './Footer';
-import Landing from './Landing';
-import ArticlePreview from './ArticlePreview';
-import SignUp from './SignUp';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  //useRef
-  const scrollToBlogRef = useRef();
-  const scrollToHomeRef = useRef();
-  const scrollToSignUpRef = useRef();
-
-  //scroll handlers
-  const scrollToBlogHandle = () => {
-    scrollToBlogRef.current.scrollIntoView({ behavior: 'smooth' });
-  };
-
-  const scrollToSignUpHandle = () => {
-    scrollToSignUpRef.current.scrollIntoView({ behavior: 'smooth' });
-  };
-
   return (
     <ChakraProvider theme={theme}>
-      <Box backgroundColor={'yellow.300'}>
-        <Header
-          blogHandle={scrollToBlogHandle}
-          signUpHandle={scrollToSignUpHandle}
-        />
-        <Landing blogHandle={scrollToBlogHandle} blogRef={scrollToBlogRef} />
-        <ArticlePreview
-          blogRef={scrollToBlogRef}
-          signUpRef={scrollToSignUpRef}
-        />
-        {/* <Box textAlign="center" fontSize="xl">
-          <Grid minH="100vh" p={3}>
-            
-            <VStack spacing={8}></VStack>
-          </Grid>
-        </Box> */}
-        <SignUp />
-        <Footer />
-      </Box>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </ChakraProvider>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react';
+import { Box } from '@chakra-ui/react';
+import Header from './Header';
+import Landing from './Landing';
+import ArticlePreview from './ArticlePreview';
+import SignUp from './SignUp';
+import Footer from './Footer';
+
+const Home = () => {
+  //useRef
+  const scrollToBlogRef = useRef();
+  const scrollToHomeRef = useRef();
+  const scrollToSignUpRef = useRef();
+
+  //scroll handlers
+  const scrollToBlogHandle = () => {
+    scrollToBlogRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const scrollToSignUpHandle = () => {
+    scrollToSignUpRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <div>
+      <Box backgroundColor={'yellow.300'}>
+        <Header
+          blogHandle={scrollToBlogHandle}
+          signUpHandle={scrollToSignUpHandle}
+        />
+        <Landing blogHandle={scrollToBlogHandle} blogRef={scrollToBlogRef} />
+        <ArticlePreview
+          blogRef={scrollToBlogRef}
+          signUpRef={scrollToSignUpRef}
+        />
+        <SignUp />
+        <Footer />
+      </Box>
+    </div>
+  );
+};
+
+export default Home;
